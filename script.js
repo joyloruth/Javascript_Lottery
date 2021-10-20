3c87709298315dc1603db86927a2f504941630c7
joyloruth <joyloruth@gmail.com>
2021-10-20 15:44:05 -0400

    added logic for lottery random number shuffle

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function random(max){
+  return Math.floor(Math.random(max) * max);
+ }
+  const counter = document.querySelector('.counter');
+  const speed = 200;
+ 
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target') * random(100) ;
+    const count = +counter.innerText;
+    const inc = target/speed;
+    
+    if(count<target)
+    {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 9);
+    }
+ 
+    else
+    {
+      count.innerText =  target;
+    }
+ 
+    switch(true)
+      {
+        case (0 <= count && count <= 1000):
+          setTimeout(function(){ document.getElementById('alert-win').style.display = 'block'; }, 1000);
+          setTimeout(function(){ document.getElementById('alert-lose').style.display = 'none'; }, 1000);
+          break;
+       case (count >= 1000):
+           setTimeout(function(){ document.getElementById('alert-lose').style.display = 'block'; }, 1000);
+           setTimeout(function(){ document.getElementById('alert-win').style.display = 'none'; }, 1002);
+          break;
+ 
+      }
+    }
+ 
+ function closeAlert(){
+   var alertbox = document.getElementById("alert-lose");
+   alertbox.remove();
+   window.location.reload(true); 
+ }
+ function closeAlertWin(){
+   var alertboxWin = document.getElementById("alert-win");
+   alertboxWin.remove();
+   window.location.reload(true); 
+ }
+ 
+ 
